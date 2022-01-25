9232e4d2feb5c6a250a1e379d813787b63826693
skdksldk <tnwns1485@naver.com>
2022-01-25 18:49:48 +0900

    Reinstall react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Main from './pages/Main';
+import List from './pages/List';
+import Community from './pages/Community';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Route path="/" exact component={Main} />
+      <Route path="/list" exact component={List} />
+      <Route path="/community" exact component={Community} />
+    </BrowserRouter>
+  );
+};
 
 export default App;
