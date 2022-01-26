3c92f6fa415849562fda6321120fc080499e7ab7
skdksldk <tnwns1485@naver.com>
2022-01-26 11:41:50 +0900

    Update ListItem

--- src/pages/ListItem.jsx
+++ src/pages/ListItem.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { List, Space } from 'antd';
-import { HeartTwoTone, MessageOutlined, LikeOutlined } from '@ant-design/icons';
+import { HeartTwoTone, MessageOutlined } from '@ant-design/icons';
 import axios from 'axios';
 
 const ListItem = () => {
- 
+ // 추후 좋아요 & 댓글 수 반영하기
 
   const IconText = ({ icon, text }) => (
     <Space>
@@ -15,6 +15,7 @@ const ListItem = () => {
 
   const [mountains, setMountains] = useState(undefined);
 
+   // 임시 데이터
   useEffect(() => {
     let completed = false;
     const getMountains = async () => {
@@ -34,12 +35,7 @@ const ListItem = () => {
       <List
         itemLayout="vertical"
         size="large"
-        pagination={{
-          onChange: page => {
-            console.log(page);
-          },
-          pageSize: 3,
-        }}
+        pagination={{ pageSize: 3 }}
         dataSource={mountains}
         renderItem={item => (
           <List.Item
@@ -50,11 +46,7 @@ const ListItem = () => {
                 text="156"
                 key="list-vertical-star-o"
               />,
-              <IconText
-                icon={LikeOutlined}
-                text="156"
-                key="list-vertical-like-o"
-              />,
+            
               <IconText
                 icon={MessageOutlined}
                 text="2"
@@ -64,7 +56,7 @@ const ListItem = () => {
             extra={<img width={272} alt="mountain" src={item.imgUrl[0]} />}
           >
             <List.Item.Meta
-              title={<a href={item.orgUrl}>{item.mountainName}</a>}
+              title={<a href="temp">{item.mountainName}</a>}
               description={item.addressDetail}
             />
             {item.mountainInfo}
