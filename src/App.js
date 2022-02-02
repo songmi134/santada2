ab8459299e4a3c17b54d9416fc77c58ad5d15af1
songmi134 <songmi134@gmail.com>
2022-02-03 00:57:43 +0900

    Add Login component with Context API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Landing from './pages/Landing/Landing';
-import Search from './pages/Search/Search';
-import Community from './pages/Community/Community';
-
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Landing from "./pages/Landing/Landing";
+import Search from "./pages/Search/Search";
+import Community from "./pages/Community/Community";
+import Login from "./pages/Login/Login";
+import AuthProvider from "./components/login/AuthProvider";
 
 const App = () => {
   return (
-    <BrowserRouter basename="mountain">
-      <Navbar />
-      <Route path="/" exact component={Landing} />
-      <Route path="/search" exact component={Search} />
-      <Route path="/community" exact component={Community} />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter basename="mountain">
+        <Navbar />
+        <Route path="/" exact component={Landing} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/search" exact component={Search} />
+        <Route path="/community" exact component={Community} />
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
 
