6f23e437f3a7fc857262419658c0d15782649766
songmi134 <songmi134@gmail.com>
2022-02-03 00:45:00 +0900

    Add Axios module configuration

--- src/components/login/AuthProvider.js
+++ src/components/login/AuthProvider.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { auth } from "../../auth/firebaseAuth";
 import RegisterForm from "./RegisterForm";
-import { defaultHeaders } from "../../config/clientConfig";
+import { axiosInstance } from "../../config/axiosConfig";
 
 export const UserContext = React.createContext(null);
 const AuthProvider = ({ children }) => {
@@ -12,31 +12,25 @@ const AuthProvider = ({ children }) => {
     auth.onAuthStateChanged(async (firebaseUser) => {
       if (firebaseUser) {
         const token = await firebaseUser.getIdToken();
-        defaultHeaders.Authorization = `Bearer ${token}`;
-        console.log(token);
-        //https://cors-anywhere.herokuapp.com/
-        const res = await fetch("https://moutain.herokuapp.com/users/me", {
-          mode: "no-cors",
-          method: "GET",
-          headers: defaultHeaders,
-        });
-        console.log(res);
-        console.log("1 : " + res.status);
-        if (res.status === 200) {
-          console.log("2 : ");
-          const user = await res.json();
-          setUser(user);
-        } else if (res.status === 401) {
-          console.log("3 : ");
-          const data = await res.json();
-          if (data.code === "USER_NOT_FOUND") {
-            console.log("4 : ");
-            setRegisterFormOpen(true);
+        localStorage.setItem("token", "Bearer " + token);
+
+        var res;
+        try {
+          res = await axiosInstance.get("/users/me");
+          if (res.status === 200) {
+            const user = await res;
+            setUser(user);
+          }
+        } catch (e) {
+          //에러발생 시
+          if (e.response.status === 404) {
+            if (e.response.data.code === "USER_NOT_FOUND") {
+              setRegisterFormOpen(true);
+            }
           }
         }
       } else {
-        console.log("5 : ");
-        delete defaultHeaders.Authorizations;
+        localStorage.clear();
         setUser(null);
       }
     });
