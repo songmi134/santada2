44fe0fd16fc62e1849926ec7c45b5511fa21597c
songmi134 <songmi134@gmail.com>
2022-02-10 02:51:51 +0900

    Modify About.styles.js with Description, Text

--- src/pages/Landing/About.jsx
+++ src/pages/Landing/About.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useRef } from "react";
-import { Wrapper, Title } from "./About.styles";
+import { Wrapper, Description, Title, Text } from "./About.styles";
+import backImg1 from "../../src_assets/picture1.jpg";
+import backImg2 from "../../src_assets/picture2.png";
+import backImg3 from "../../src_assets/picture3.png";
 import { revealText } from "./About.animations";
 
 const About = () => {
@@ -20,6 +23,36 @@ const About = () => {
             <br />
             <br />
           </Title>
+          <Description id="about-text">
+            <img src={backImg2} alt="image" />
+            <div>
+              <Text>
+                <strong>하나!</strong>
+                <br />
+                내가 가고싶은 산 검색
+              </Text>
+            </div>
+          </Description>
+          <Description id="about-text">
+            <div>
+              <Text>
+                <strong>둘!</strong>
+                <br />
+                내가 가고싶은 산 검색
+              </Text>
+            </div>
+            <img src={backImg1} alt="image" />
+          </Description>
+          <Description id="about-text">
+            <img src={backImg3} alt="image" />
+            <div>
+              <Text>
+                <strong>셋!</strong>
+                <br />
+                내가 가고싶은 산 검색
+              </Text>
+            </div>
+          </Description>
         </div>
       </Wrapper>
     </>
--- src/pages/Landing/About.styles.js
+++ src/pages/Landing/About.styles.js
@@ -1,6 +1,26 @@
 import styled from "styled-components";
 import backImg from "../../src_assets/back.jpg";
 
+export const Description = styled.p`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  font-family: "Merriweather", "Spoqa Han Sans";
+  font-weight: 300;
+  opacity: 0;
+  transform: translateY(60px);
+  padding-right: 5%;
+  padding-left: 5%;
+
+  img {
+    width: 50%;
+    margin: 1rem;
+    margin-bottom: 2rem;
+  }
+`;
+
 export const Title = styled.div`
   font-family: "Merriweather", "Spoqa Han Sans";
   font-style: italic;
@@ -17,6 +37,11 @@ export const Title = styled.div`
   background: url(${backImg});
   background-size: cover;
   background-repeat: no-repeat;
+`;
+
+export const Text = styled.div`
+  margin: 3rem;
+  font-size: 2.5rem;
 `;
 
 export const Wrapper = styled.section`
