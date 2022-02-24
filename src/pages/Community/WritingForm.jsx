def283d17a935812a3eac29f40df8e3b95886883
skdksldk <tnwns1485@naver.com>
2022-02-24 16:56:40 +0900

    Add Infinite scroll to Community

--- src/pages/Community/WritingForm.jsx
+++ src/pages/Community/WritingForm.jsx
@@ -3,17 +3,33 @@ import { Form, Input, Row, Radio } from 'antd';
 import { FormContainer, FormInput, FormButton } from './Community.style';
 import axios from 'axios';
 import moment from 'moment';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 const WritingForm = () => {
-  const [categories, setCategories] = useState(undefined);
-  const [category, setCategory] = useState(undefined);
   const history = useHistory();
-  
+  const params = useParams();
+  const postId = params.id;
+  const [post, setPost] = useState(undefined);
+  const [categories, setCategories] = useState(undefined);
+
+  useEffect(() => {
+    let completed = false;
+    const getPost = async () => {
+      const response = await axios.get(`/communities/${postId}`);
+      if (!completed) {
+        setPost(response.data);
+      }
+    };
+    getPost();
+    return () => {
+      completed = true;
+    };
+  }, []);
+
   useEffect(() => {
     let completed = false;
     const getMountains = async () => {
-      const response = await axios.get('http://localhost:4000/category');
+      const response = await axios.get('/categories');
       if (!completed) {
         setCategories(response.data);
       }
@@ -24,43 +40,65 @@ const WritingForm = () => {
     };
   }, []);
 
-  const handleCategoryChange = v => setCategory(v.target.value);
-
   const onFinish = async values => {
-    const createdAt = moment().format('YYYY.MM.DD HH:mm:ss');
+    // TODO: 로그인 붙인 이후, createAt 제거한 후 잘 되는지 확인
+    const createdAt = moment().format('YYYY.MM.DD HH:mm:ss'); //백엔드 DB 저장시 - 백엔드에서 할지 or 프론트에서 한다면 어떤 format으로 할지
+    // TODO : 형식 맞추기
     const newPost = {
       ...values,
       createdAt,
-      category,
       viewCount: 0,
-      writer: { name: '닉네임' },
+      user: { name: '닉네임' },
     };
+    console.log(newPost);
     try {
-      await axios.post('http://localhost:4000/community', newPost);
-      history.push('/community');
+      await axios.post('/communities', newPost);
+      history.push('/community'); // 현재 생성된 포스트의 id를 알아내서 해당 detail 페이지로 이동시키기
     } catch (err) {
       console.log(err);
     }
   };
 
+  const handleEdit = async values => {
+    const updatedAt = moment().format('YYYY.MM.DD HH:mm:ss');
+    const updatedPost = { ...values, updatedAt };
+    try {
+      await axios.patch(
+        `http://localhost:4000/community/${postId}`,
+        updatedPost
+      );
+      history.push(`/community/${postId}`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  console.log('POST', post);
   return (
     <Row>
-       <FormContainer onFinish={onFinish}>
-        <Radio.Group
-          defaultValue={1}
-          buttonStyle="solid"
-          onChange={handleCategoryChange}
+      <FormContainer
+        onFinish={post ? handleEdit : onFinish}
+        fields={[
+          { name: ['title'], value: post?.title },
+          { name: ['content'], value: post?.content },
+          { name: ['category'], value: post?.category },
+        ]}
+      >
+        <Form.Item
+          name="category"
+          rules={[{ required: true, message: '카테고리를 선택하세요!' }]}
         >
-          {categories ? (
-            categories.map(v => (
-              <Radio.Button key={v.cateId} value={v.cateName}>
-              {v.cateName}
-            </Radio.Button>
-          ))
-        ) : (
-          <Row>Loading...</Row>
-        )}
-      </Radio.Group>
+          <Radio.Group buttonStyle="solid">
+            {categories ? (
+              categories.content.map(v => (
+                <Radio.Button key={v.cateId} value={v.cateName}>
+                  {v.cateName}
+                </Radio.Button>
+              ))
+            ) : (
+              <Row>Loading...</Row>
+            )}
+          </Radio.Group>
+        </Form.Item>
         <Form.Item
           name="title"
           rules={[{ required: true, message: '제목을 입력하세요!' }]}
