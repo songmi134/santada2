78e8f5b3f02fe77ce0e03604f399d67ae4ff4c2e
skdksldk <tnwns1485@naver.com>
2022-02-28 17:35:52 +0900

    Update writingform page : axios

--- src/pages/Community/WritingForm.jsx
+++ src/pages/Community/WritingForm.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Form, Input, Row, Radio } from 'antd';
 import { FormContainer, FormInput, FormButton } from './Community.style';
 import axios from 'axios';
+import { axiosInstance } from "../../config/axiosConfig";
 import moment from 'moment';
 import { useHistory, useParams } from 'react-router-dom';
 
@@ -11,25 +12,11 @@ const WritingForm = () => {
   const postId = params.id;
   const [post, setPost] = useState(undefined);
   const [categories, setCategories] = useState(undefined);
-
-  useEffect(() => {
-    let completed = false;
-    const getPost = async () => {
-      const response = await axios.get(`/communities/${postId}`);
-      if (!completed) {
-        setPost(response.data);
-      }
-    };
-    getPost();
-    return () => {
-      completed = true;
-    };
-  }, []);
-
+ 
   useEffect(() => {
     let completed = false;
     const getMountains = async () => {
-      const response = await axios.get('/categories');
+      const response = await axiosInstance.get('/categories');
       if (!completed) {
         setCategories(response.data);
       }
@@ -39,33 +26,27 @@ const WritingForm = () => {
       completed = true;
     };
   }, []);
+ 
 
   const onFinish = async values => {
-    // TODO: 로그인 붙인 이후, createAt 제거한 후 잘 되는지 확인
-    const createdAt = moment().format('YYYY.MM.DD HH:mm:ss'); //백엔드 DB 저장시 - 백엔드에서 할지 or 프론트에서 한다면 어떤 format으로 할지
-    // TODO : 형식 맞추기
     const newPost = {
-      ...values,
-      createdAt,
-      viewCount: 0,
-      user: { name: '닉네임' },
+      content: values.content,
+      cateId: values.cateId,
+      title: values.title, 
     };
     console.log(newPost);
     try {
-      await axios.post('/communities', newPost);
+      await axiosInstance.post('/communities', newPost);
       history.push('/community'); // 현재 생성된 포스트의 id를 알아내서 해당 detail 페이지로 이동시키기
     } catch (err) {
       console.log(err);
     }
   };
-
+  
   const handleEdit = async values => {
-    const updatedAt = moment().format('YYYY.MM.DD HH:mm:ss');
-    const updatedPost = { ...values, updatedAt };
     try {
-      await axios.patch(
-        `http://localhost:4000/community/${postId}`,
-        updatedPost
+      await axiosInstance.patch(
+        
       );
       history.push(`/community/${postId}`);
     } catch (err) {
@@ -80,17 +61,17 @@ const WritingForm = () => {
         fields={[
           { name: ['title'], value: post?.title },
           { name: ['content'], value: post?.content },
-          { name: ['category'], value: post?.category },
+          { name: ['cateId'], value: post?.cateId },
         ]}
       >
         <Form.Item
-          name="category"
+          name="cateId"
           rules={[{ required: true, message: '카테고리를 선택하세요!' }]}
         >
           <Radio.Group buttonStyle="solid">
             {categories ? (
               categories.content.map(v => (
-                <Radio.Button key={v.cateId} value={v.cateName}>
+                <Radio.Button key={v.cateId} value={v.cateId}>
                   {v.cateName}
                 </Radio.Button>
               ))
@@ -98,6 +79,8 @@ const WritingForm = () => {
               <Row>Loading...</Row>
             )}
           </Radio.Group>
+    
+    
         </Form.Item>
         <Form.Item
           name="title"
@@ -128,5 +111,6 @@ const WritingForm = () => {
     </Row>
   );
 };
+
 
 export default WritingForm;
