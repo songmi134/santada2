9ef75cb0e085602e8a7d877baadfaedf0f10bd3f
songmi134 <songmi134@gmail.com>
2022-03-01 15:16:24 +0900

    Add MyComments component with axiosInstance

--- src/pages/My/My.jsx
+++ src/pages/My/My.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Tabs } from "antd";
 import MyMountains from "./MyMountains";
+import MyComments from "./MyComments";
 
 const My = () => {
   const { TabPane } = Tabs;
@@ -9,6 +10,9 @@ const My = () => {
       <Tabs defaultActiveKey="1" centered>
         <TabPane tab="찜한 산" key="1">
           <MyMountains />
+        </TabPane>
+        <TabPane tab="작성한 댓글" key="3">
+          <MyComments />
         </TabPane>
       </Tabs>
     </>
